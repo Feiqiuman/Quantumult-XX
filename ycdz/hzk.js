0a1dcba97c58873d95c2aa13e3a449d47b539300
Feiqiuman <68541591+Feiqiuman@users.noreply.github.com>
2024-02-02 22:09:13 +0800

    Update hzk.js

--- ycdz/hzk.js
+++ ycdz/hzk.js
@@ -15,11 +15,11 @@ TG频道群：https://t.me/py996
 
 [rewrite_local]
 
-^http[s]?:\/\/www.zhongkao66.com url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/hzk.js
+^http[s]?:\/\/www.gaokao66.com url script-response-body https://raw.githubusercontent.com/Feiqiuman/Quantumult-XX/main/ycdz/hzk.js
 
 [mitm] 
 
-hostname = www.zhongkao66.com
+hostname = www.gaokao66.com
 
 *******************************/
 
